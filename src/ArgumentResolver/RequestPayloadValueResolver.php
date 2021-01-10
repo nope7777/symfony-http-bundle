b7e145264da3c5a67d7227bc04db9da1f465a1a3
Vladimir <nope7777@gmail.com>
2021-01-10 15:10:10 +0200

    wip

---
+++ src/ArgumentResolver/RequestPayloadValueResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N7\SymfonyHttp\ArgumentResolver;
+
+use N7\SymfonyHttp\Interfaces\RequestPayloadInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+final class RequestPayloadValueResolver implements ArgumentValueResolverInterface
+{
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        $type = $argument->getType();
+        if (! $type || ! class_exists($type)) {
+            return false;
+        }
+
+        $interfaces = class_implements($type);
+
+        return array_key_exists(RequestPayloadInterface::class, $interfaces);
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument)
+    {
+        throw new \RuntimeException('it works');
+        yield $this->security->getUser();
+    }
+}
---
+++ src/N7SymfonyHttp.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N7\SymfonyHttp;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class N7SymfonyHttp extends Bundle
+{
+
+}
